e65658e152180506c501872159182848c8096cdb
App Generator <51070104+app-generator@users.noreply.github.com>
2022-12-31 07:09:08 +0200

    Update run.py

--- run.py
+++ run.py
@@ -35,7 +35,6 @@ if DEBUG:
     app.logger.info('DEBUG            = ' + str(DEBUG)             )
     app.logger.info('Page Compression = ' + 'FALSE' if DEBUG else 'TRUE' )
     app.logger.info('DBMS             = ' + app_config.SQLALCHEMY_DATABASE_URI)
-    app.logger.info('ASSETS_ROOT      = ' + app_config.ASSETS_ROOT )
 
 if __name__ == "__main__":
     app.run()
